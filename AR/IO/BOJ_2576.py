cad5cf221c6cdcbeb30fb7d606bcc34af7a52809
DaewonSeo <saz0549@naver.com>
2021-12-17 23:06:29 +0900

    solved boj 2576

--- AR/IO/BOJ_2576.py
+++ AR/IO/BOJ_2576.py
@@ -1,1 +1,7 @@
-#
+numbers = [input() for _ in range(7)]
+odds = sorted([ int(num) for num in numbers if int(num) % 2 != 0 ])
+
+if len(odds) == 0:
+    print(-1)
+else:
+    print(sum(odds), odds[0], sep='\n') 
